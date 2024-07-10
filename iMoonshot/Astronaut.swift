40517329fb02438f0f6c9b216ae124db83d4d07f
GarifullinII <ildaridrisovich@gmail.com>
2024-07-10 19:25:26 +0400

    Initial Commit

---
+++ iMoonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  iMoonshot
+//
+//  Created by Ildar Garifullin on 10/07/2024.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
---
+++ iMoonshot/iMoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMoonshotApp.swift
+//  iMoonshot
+//
+//  Created by Ildar Garifullin on 09/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct iMoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
