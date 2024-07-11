639caba18d313b6d9ff8c5c97b0b7de2ec1bc57d
GarifullinII <ildaridrisovich@gmail.com>
2024-07-11 16:26:39 +0400

    add all view

---
+++ iMoonshot/MissionView.swift
@@ -0,0 +1,18 @@
+//
+//  MissionView.swift
+//  iMoonshot
+//
+//  Created by Ildar Garifullin on 11/07/2024.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MissionView()
+}
